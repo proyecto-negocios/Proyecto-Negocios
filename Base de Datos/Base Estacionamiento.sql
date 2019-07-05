c94c2c99b446e2fefd4544527abaf07079974d3e
DouglasWZ <51092517+DouglasWZ@users.noreply.github.com>
2019-07-05 10:23:52 -0600

    Add files via upload

---
+++ Base de Datos/Base Estacionamiento.sql
@@ -0,0 +1,43 @@
+USE tempdb
+go
+
+IF NOT EXISTS(SELECT * FROM sys.databases WHERE [name] = 'Estacionamiento')
+	BEGIN
+		CREATE DATABASE Estacionamiento
+	END
+GO
+
+CREATE SCHEMA Park
+GO
+
+USE Estacionamiento
+GO
+
+-- Creacion de las Tablas 
+CREATE TABLE Park.Tipo_Carro
+(
+	id INT IDENTITY (1,1) NOT NULL
+	CONSTRAINT PK_Tipo_Carro_id PRIMARY KEY CLUSTERED,
+	tipo_carro NVARCHAR (30) NOT NULL 
+)
+GO
+
+CREATE TABLE Park.Carro
+(
+	id INT IDENTITY (1,1) NOT NULL
+	CONSTRAINT PK_Carro_id PRIMARY KEY CLUSTERED,
+	placa NVARCHAR (30) NOT NULL,
+	fecha_ingreso DATETIME,
+	fecha_salida DATETIME	
+)
+GO
+
+CREATE TABLE Park.Cobro
+(
+	id_cobro INT IDENTITY (1,1) NOT NULL,
+	id INT IDENTITY (1,1) NOT NULL,
+	fecha_ingreso DATETIME,
+	fecha_salida DATETIME	
+)
+
+
